e1b7e8a90201f0a76f8c312b8de80722e6b0032b
Admazzola <admazzola@gmail.com>
2023-03-12 13:12:06 -0400

    update manifest

--- services/tasks/generate-manifests.ts
+++ services/tasks/generate-manifests.ts
@@ -36,7 +36,7 @@ function handleRow( row :any  ){
 
 
         name,
-        description:`0xJPEGs are a celebration of the lasting legacy of decentralized ERC20 tokens on Ethereum Mainnet.  0xBTC is the faithful implementation of the bitcoin protocol as a trustless, immutable Ethereum smart contract. It has credible neutrality due to proof-of-work issuance and a 21 million coin cap within the Ethereum EVM. This enables dapp composability and decentralized-exchange trading.\n\n\n0xBitcoin is an open community project with decentralized development. No person or organization has special authority or control over 0xbtc. The 0xJPEGs artworks honor the decentralized, composable, and permissionless nature of the technology as non-upgradeable non-fungible tokens built in a custom smart contract.\n\n\nFind more at https://0xbitcoin.org\n\n\nImage: ${imageUrl}`,
+        description:`0xJPEGs are a celebration of decentralized and credibly-neutral assets on Ethereum Mainnet.  0xBTC is the faithful implementation of the bitcoin protocol as a trustless, immutable Ethereum smart contract. It has credible neutrality due to proof-of-work issuance and a 21 million coin cap within the Ethereum EVM. This enables dapp composability and decentralized-exchange trading.\n\n\n0xBitcoin is an open community project with decentralized development. No person or organization has special authority or control over 0xbtc. The 0xJPEGs artworks honor the decentralized, composable, and permissionless nature of the technology as non-upgradeable non-fungible tokens built in a custom smart contract.\n\n\nFind more at https://0xbitcoin.org\n\n\nImage: ${imageUrl}`,
         external_url:"https://0xjpegs.com",
         image:imageUrl,
 
